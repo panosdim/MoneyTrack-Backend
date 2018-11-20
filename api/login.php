31742ed88e1525ff64eb9e294443a67e5afaeed0
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2018-11-20 11:06:56 +0200

    Don't DELETE all session token when authentication fails.

--- api/login.php
+++ api/login.php
@@ -10,68 +10,69 @@ $data = json_decode(file_get_contents('php://input'), true);
 $username = $data["username"];
 $password = $data["password"];
 
-function generateToken($length = 20) {
-	return bin2hex(random_bytes($length));
+function generateToken($length = 20)
+{
+    return bin2hex(random_bytes($length));
 }
 
 // Get hashed password for user
 $stmt = $db->prepare(
-	'SELECT * FROM users WHERE username = ?'
+    'SELECT * FROM users WHERE username = ?'
 );
 if ($stmt->execute([$username])) {
-	$query = $stmt->fetch();
+    $query = $stmt->fetch();
 
-	if ($query !== false) {
-		// Hashing the DB password with the salt returns the same hash
-		if (password_verify($password, $query['password'])) {
-			$series = generateToken();
-			$token = generateToken();
-			$selector = generateToken(5);
-			$hashedToken = hash('sha256', $token);
+    if ($query !== false) {
+        // Hashing the DB password with the salt returns the same hash
+        if (password_verify($password, $query['password'])) {
+            $series = generateToken();
+            $token = generateToken();
+            $selector = generateToken(5);
+            $hashedToken = hash('sha256', $token);
 
-			$date = date_create('now');
-			date_add($date, date_interval_create_from_date_string('30 days'));
+            $date = date_create('now');
+            date_add($date, date_interval_create_from_date_string('30 days'));
 
-			$stmt = $db->prepare(
-				'INSERT INTO `auth_tokens` (`selector`, `series`, `hashedToken`, `username`, `user_id`, `expire`) VALUES (?, ?, ?, ?, ?, ?)'
-			);
+            $stmt = $db->prepare(
+                'INSERT INTO `auth_tokens` (`selector`, `series`, `hashedToken`, `username`, `user_id`, `expire`) VALUES (?, ?, ?, ?, ?, ?)'
+            );
 
-			if ($stmt->execute([$selector, $series, $hashedToken, $username, $query['id'], date_format($date, 'Y-m-d')])) {
-				$data = [
-					"series" => $series,
-					"token" => $token,
-					"selector" => $selector,
-				];
-			}
-			// Authentication successful - Set session
-			echo json_encode([
-				"success" => true,
-				"message" => "Login was successful.",
-				"data" => $data,
-			]);
-			$_SESSION['userId'] = $query['id'];
-			$_SESSION['username'] = $username;
-		} else {
-			// Authentication was not successful. Inform user with message.
-			echo json_encode([
-				"success" => false,
-				"message" => "Login was not successful.",
-				"data" => "",
-			]);
-		}
-	} else {
-		// Authentication was not successful. Inform user with message.
-		echo json_encode([
-			"success" => false,
-			"message" => "Login was not successful.",
-			"data" => "",
-		]);
-	}
+            if ($stmt->execute([$selector, $series, $hashedToken, $username, $query['id'], date_format($date, 'Y-m-d')])) {
+                $data = [
+                    "series" => $series,
+                    "token" => $token,
+                    "selector" => $selector,
+                ];
+            }
+            // Authentication successful - Set session
+            echo json_encode([
+                "success" => true,
+                "message" => "Login was successful.",
+                "data" => $data,
+            ]);
+            $_SESSION['userId'] = $query['id'];
+            $_SESSION['username'] = $username;
+        } else {
+            // Authentication was not successful. Inform user with message.
+            echo json_encode([
+                "success" => false,
+                "message" => "Login was not successful.",
+                "data" => "",
+            ]);
+        }
+    } else {
+        // Authentication was not successful. Inform user with message.
+        echo json_encode([
+            "success" => false,
+            "message" => "Login was not successful.",
+            "data" => "",
+        ]);
+    }
 } else {
-	// DB interaction was not successful. Inform user with message.
-	echo json_encode([
-		"success" => false,
-		"message" => "Problem executing statement in DB. Try again later.",
-		"data" => "",
-	]);
+    // DB interaction was not successful. Inform user with message.
+    echo json_encode([
+        "success" => false,
+        "message" => "Problem executing statement in DB. Try again later.",
+        "data" => "",
+    ]);
 }
--- api/session.php
+++ api/session.php
@@ -11,92 +11,86 @@ $selector = $data["selector"];
 $series = $data["series"];
 $token = $data["token"];
 
-function generateToken($length = 20) {
-	return bin2hex(random_bytes($length));
+function generateToken($length = 20)
+{
+    return bin2hex(random_bytes($length));
 }
 
 // Get hashed token for user
 $stmt = $db->prepare(
-	'SELECT `id`, `username`, `user_id`, `expire`, `series`, `hashedToken`, `expire` FROM `auth_tokens` WHERE selector = ?'
+    'SELECT `id`, `username`, `user_id`, `expire`, `series`, `hashedToken` FROM `auth_tokens` WHERE selector = ?'
 );
 
 if ($stmt->execute([$selector])) {
-	$query = $stmt->fetch();
+    $query = $stmt->fetch();
 
-	if ($query !== false) {
-		// Check if authentication token is expired
-		if (time() < strtotime($query['expire'])) {
-			// Check for valid series
-			if (hash_equals($series, $query['series'])) {
-				$hashedToken = hash('sha256', $token);
-				// Check for valid token
-				if (hash_equals($hashedToken, $query['hashedToken'])) {
-					$_SESSION['userId'] = $query['user_id'];
-					$_SESSION['username'] = $query['username'];
+    if ($query !== false) {
+        // Check if authentication token is expired
+        if (time() < strtotime($query['expire'])) {
+            // Check for valid series
+            if (hash_equals($series, $query['series'])) {
+                $hashedToken = hash('sha256', $token);
+                // Check for valid token
+                if (hash_equals($hashedToken, $query['hashedToken'])) {
+                    $_SESSION['userId'] = $query['user_id'];
+                    $_SESSION['username'] = $query['username'];
 
-					// Generate a new token and update DB
-					$token = generateToken();
-					$hashedToken = hash('sha256', $token);
+                    // Generate a new token and update DB
+                    $token = generateToken();
+                    $hashedToken = hash('sha256', $token);
 
-					$stmt = $db->prepare(
-						'UPDATE `auth_tokens` SET `hashedToken` = ? WHERE `id` = ?'
-					);
+                    $stmt = $db->prepare(
+                        'UPDATE `auth_tokens` SET `hashedToken` = ? WHERE `id` = ?'
+                    );
 
-					if ($stmt->execute([$hashedToken, $query['id']])) {
-						echo json_encode([
-							"success" => true,
-							"message" => "Session is alive.",
-							"data" => $token,
-						]);
-					}
-				} else {
-					// Invalid token when series and selector are valid means cookie stolen attack
-					// Delete ALL authentication tokens for the user
+                    if ($stmt->execute([$hashedToken, $query['id']])) {
+                        echo json_encode([
+                            "success" => true,
+                            "message" => "Session is alive.",
+                            "data" => $token,
+                        ]);
+                    }
+                } else {
+                    // Invalid token when series and selector are valid means cookie stolen attack
+                    // Unset all of the session variables.
+                    $_SESSION = [];
 
-					// Unset all of the session variables.
-					$_SESSION = [];
+                    if ($stmt->execute([$query['username']])) {
+                        echo json_encode([
+                            "success" => false,
+                            "message" => "Session is not alive.",
+                            "data" => "",
+                        ]);
+                    }
 
-					// Delete authentication token from DB
-					$stmt = $db->prepare(
-						'DELETE FROM `auth_tokens` WHERE `username` = ?'
-					);
-
-					if ($stmt->execute([$query['username']])) {
-						echo json_encode([
-							"success" => false,
-							"message" => "Session is not alive.",
-							"data" => "",
-						]);
-					}
-
-					// Finally, destroy the session.
-					session_destroy();
-				}
-			} else {
-				echo json_encode([
-					"success" => false,
-					"message" => "Session is not alive.",
-					"data" => "",
-				]);
-			}
-		} else {
-			echo json_encode([
-				"success" => false,
-				"message" => "Session is not alive.",
-				"data" => "",
-			]);
-		}
-	} else {
-		echo json_encode([
-			"success" => false,
-			"message" => "Session is not alive.",
-			"data" => "",
-		]);
-	}
+                    // Finally, destroy the session.
+                    session_destroy();
+                }
+            } else {
+                echo json_encode([
+                    "success" => false,
+                    "message" => "Session is not alive.",
+                    "data" => "",
+                ]);
+            }
+        } else {
+            echo json_encode([
+                "success" => false,
+                "message" => "Session is not alive.",
+                "data" => "",
+            ]);
+        }
+    } else {
+        echo json_encode([
+            "success" => false,
+            "message" => "Session is not alive.",
+            "data" => "",
+        ]);
+    }
 } else {
-	echo json_encode([
-		"success" => false,
-		"message" => "Session is not alive.",
-		"data" => "",
-	]);
+    echo json_encode([
+        "success" => false,
+        "message" => "Session is not alive.",
+        "data" => "",
+    ]);
 }
